f2d55f73e6e8191c279cce6dd08594cff7d366a2
Ian Matthews <ianlennymatthews@gmail.com>
2019-08-09 06:28:30 -0400

    Refactor to  use axios.spread to  make requessts parallel and modular

--- server/index.js
+++ server/index.js
@@ -12,47 +12,97 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files found in dist folder
 app.use('/', express.static(path.join(__dirname, '/../client/dist')));
 
-let getCanadaPostAPI = `https://7ywg61mqp6.execute-api.us-east-1.amazonaws.com/prod/rates/${
-  req.body.postal
-}`;
+let canadaPostAPI =
+  'https://7ywg61mqp6.execute-api.us-east-1.amazonaws.com/prod/rates/';
 
-let getBoxKnightAPI = `https://lo2frq9f4l.execute-api.us-east-1.amazonaws.com/prod/rates/${
-  req.body.postal
-}`;
+let boxKnightAPI =
+  'https://lo2frq9f4l.execute-api.us-east-1.amazonaws.com/prod/rates/';
+
+function getCanadaRates(postalCode) {
+  return axios.get(canadaPostAPI + postalCode);
+}
+
+function getBoxKnightRates(postalCode) {
+  return axios.get(boxKnightAPI + postalCode);
+}
 
 app.post('/getBestShippingRate', (req, res) => {
-  let canadaData = [];
+  let {
+    address_line_one,
+    address_line_two,
+    city,
+    province,
+    postalCode,
+    country
+  } = req.body;
 
+  let canadaData = [];
   let boxKnightData = [];
 
   axios
-    .get(getCanadaPostAPI)
-    .then(data => {
-      canadaData = data;
-    })
+    .all([getCanadaRates(postalCode), getBoxKnightRates(postalCode)])
+    .then(
+      axios.spread((canadaResponseData, boxKnightResponseData) => {
+        canadaData = canadaResponseData;
+        boxKnightData = boxKnightResponseData;
+
+        let lowestRate = getLowestCostOrDate(canadaData, boxKnightData);
+        res.send(lowestRate);
+
+        if (sendViaCanada(lowestRate)) {
+          axios
+            .post(canadaPostAPI + 'shipments', {
+              rate_id: lowestRate.id,
+              destination: {
+                address_line_one: address_line_one,
+                address_line_two: address_line_two,
+                city: city,
+                province: province,
+                postalCode: postalCode,
+                country: country
+              }
+            })
+            .catch(err => {
+              console.log(
+                'There was an error while making post request to Canada Post API: ',
+                err
+              );
+            });
+        } else {
+          axios
+            .post(boxKnightAPI + 'shipments', {
+              rate_id: lowestRate.id,
+              destination: {
+                address_line_one: address_line_one,
+                address_line_two: address_line_two,
+                city: city,
+                province: province,
+                postalCode: postalCode,
+                country: country
+              }
+            })
+            .catch(err => {
+              console.log(
+                'There was an error while making post request to BoxKnight API: ',
+                err
+              );
+            });
+        }
+      })
+    )
     .catch(err => {
       console.log(
-        'An error returned from making a request to CanadaPost API: ',
-        err
-      );
-    });
-
-  axios
-    .get(getBoxKnightAPI)
-    .then(data => {
-      boxKnightData = data;
-
-      getLowestCostOrDate(canadaData, boxKnightData);
-    })
-    .catch(err => {
-      console.log(
-        'An error returned from making a request to BoxKnight API: ',
+        'And error turned from making initial get request to BoxKnight API || Canada Post API: ',
         err
       );
     });
 });
 
-parseRates = (array) => {};
+sendViaCanada = obj => {
+  return obj.sendCanada ? true : false;
+};
+
+parseRates = array => {};
 
 getLowestCostOrDate = (set1, set2) => {
   let lowest = [];
@@ -62,8 +112,21 @@ getLowestCostOrDate = (set1, set2) => {
 
   if (lowest[0].price === lowest[1].price) {
     // return rate with lowest shipping day
-  } else(true){
+
+    if (lowest[0].estimated_days < lowest[1].estimated_days) {
+      lowest[0].sendCanada = true;
+      return lowest[0];
+    } else {
+      lowest[1].sendCanada = false;
+      return lowest[1];
+    }
+  } else if (lowest[0].price < lowest[1].price) {
     //check which has lowest price and return
+    lowest[0].sendCanada = true;
+    return lowest[0];
+  } else {
+    lowest[1].sendCanada = true;
+    return lowest[1];
   }
 };
 
