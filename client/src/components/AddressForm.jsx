50c32cbf8b2dd6b44dae7657e529591421a919d4
Ian Matthews <ianlennymatthews@gmail.com>
2019-08-10 12:16:03 -0400

    Finally refixing the webpack config file

---
+++ client/src/components/AddressForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+class AddressForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div> Hello World</div>;
+  }
+}
+
+export default AddressForm;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import AddressForm from './components/AddressForm.jsx';
 import axios from 'axios';
 
 class App extends React.Component {
